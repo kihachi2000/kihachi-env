e2cf9464d54d16a21bebad387b55bfdb2b1b2183
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2024-12-03 16:53:05 +0900

    feat: add dev.sh

---
+++ dev.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+OPT=(
+    "-it"
+    "--rm"
+    "--workdir" "/root/virtual-home"
+    "-e" "HOME=/root"
+    "-v" "$(pwd):/root/virtual-home"
+)
+
+# Mac以外のときはユーザーidを変更する
+# https://qiita.com/s10akir/items/19e130682204e4dbbf3b
+if [ "$(uname)" != "Darwin" ]; then
+    OPT+=(
+        "-u" "$(id -u):$(id -g)"
+    )
+fi
+
+docker build --no-cache -t kihachi-env:dev . && docker run "${OPT[@]}" kihachi-env:dev "$@"
